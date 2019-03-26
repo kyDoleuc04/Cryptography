4b3a4d88a9fc5ee50d0e6ac50178b0d31cbb5bac
kyDoleuc04 <46973452+kyDoleuc04@users.noreply.github.com>
2019-03-26 14:53:06 -0400

    Updated from Brython Server: 3/26/2019 2:53:05 PM

--- cryptography.py
+++ cryptography.py
@@ -12,11 +12,11 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 help = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-a == "yes"
+a = "yes"
 num=0
 
 while a == "yes":
-    if b == "e":
+    if help == "e":
         msg=input("Message: ")
         msglist=list(msg)
         key=input("Key: ")
@@ -54,8 +54,51 @@ while a == "yes":
         print(''.join(endlist))
         num=0
         
-        redo = 'yes'
-    
+        a = 'yes'
+    elif help == "d":
+        msg=input("Message: ")
+        msglist=list(msg)
+        key=input("Key: ")
+        keylist=list(key)
+        nummsg=[]
+        msglength=len(msglist)
+        for char in msglist:
+            nummsg.append(associations.find(char))
+        keystr = key
+        keylength=len(keystr)
+        while keylength<msglength:
+            keystr=keystr+key
+            keylength=len(keystr)
+        while keylength>msglength:
+            keystr=keystr[:-1]
+            keylength=len(keystr)
+        numkey = []
+        for char in keystr:
+            numkey.append(associations.find(char))
+        num=0
+        listsum=[]
+        while num<msglength:
+            sumlist.append(nummsg[num]+numkey[num])
+            num+=1
+        num=0
+        while num<msglength:
+            if sumlist[num]>84:
+                sumlist[num]-=85
+            num+=1
+        num=0
+        endlist=[]
+                while num<msglength:
+            endlist.append(associations[listsum[num]])
+            num+=1
+        print(''.join(endlist))
+        num=0
         
+        a = 'yes'
+    elif help == "q":
+        print("Goodbye!")
+        a = "no"
+    else:
+        print("Did not understand command, try again.")
+        a = "yes"
         
     
